93ef3d0dbf3e02d70f51cda85f9da5fb509c0b7c
Elias Rodriguez <elayasrodriguez@gmail.com>
2024-05-26 22:48:24 -0400

    Creacion del proyecto

---
+++ Venta_de_vehiculos_hyundai_bolivia/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Venta_de_vehiculos_hyundai_bolivia.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
